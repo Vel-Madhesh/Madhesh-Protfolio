d06bde448bc36b64b4918566d2104c9f104ec1c4
Vel-Madhesh <mangalesh5577@gmail.com>
2026-04-15 14:57:22 +0530

    5th commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
 import React from "react"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Routes, Route } from "react-router-dom" // ❌ remove BrowserRouter
 import Navbar from "./components/Navbar"
 import Home from "./pages/Home.jsx"
 import About from "./pages/About.jsx"
 import Projects from "./pages/Projects.jsx"
 import Contact from "./pages/Contact.jsx"
-// import Resume from "./pages/Resume.jsx"
 import Footer from "./components/Footer.jsx"
 
 function App() {
-
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-          {/* <Route path="/resume" element={<Resume />} /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </>
   )
 }
 
